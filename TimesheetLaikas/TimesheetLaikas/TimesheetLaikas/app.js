7bdf1fcf5074e43a9a24d6dfdccb25a06a66e7fd
DonatelloLTU <60147196+DonatelloLTU@users.noreply.github.com>
2020-12-08 00:48:47 -0500

    fixed some stuff

--- TimesheetLaikas/TimesheetLaikas/TimesheetLaikas/app.js
+++ TimesheetLaikas/TimesheetLaikas/TimesheetLaikas/app.js
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿const http = require('http');
+const port = process.env.PORT || 3000
 
-namespace TimesheetLaikas
-{
-    public class app
-    {
-    }
-}
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
